08564f224abba469df89f405f798e5046f8299a9
youssef bouhlal <youssef.bouhlal@webdeclic.com>
2023-10-05 23:47:17 +0100

    add the ui elements

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,30 @@
+import react from 'react';
 import './App.css';
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
 
 function App() {
+
+  const contacts = [
+    {
+        id: "1",
+        name: "John",
+        email: "john@mail.com"
+    },
+    {
+        id: "2",
+        name: "Jane",
+        email: "jane@mail.com"
+    }
+  ];
+
   return (
-    <div>Hello world</div>
+    <div className='ui container'>
+      <Header />
+      <AddContact />
+      <ContactList contacts={contacts} />
+    </div>
   );
 }
 
---
+++ src/components/ContactCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import user from '../images/user.svg';
+
+const ContactCard = (props) => {
+
+    const { id, name, email } = props.contact;
+
+    return (
+        <div className="item">
+            <img className="ui avatar image" src={user} alt="User" />
+            <div className="content">
+                <div className="header">{name}</div>
+                <div>{email}</div>
+            </div>
+            <i className="trash alternate outline icon"
+            style={{color:'red',marginTop:'6px'}}></i>
+        </div>
+    );
+
+};
+
+export default ContactCard;
